ad33c800f360f5bbf81995a0b0cc5c1c098e482a
Mark Saad <markadel@gmail.com>
2017-09-23 13:20:10 -0400

    experiment with outliers to csv

--- run_me.py
+++ run_me.py
@@ -7,6 +7,7 @@ Created on Sat Sep 23 11:28:13 2017
 
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import IsolationForest
 
 """
 def read_csv(filepath):
@@ -15,6 +16,7 @@ def read_csv(filepath):
 """
 def read_csv(filepath):
     df = pd.read_csv(filepath)
+    df.fillna(0, inplace=True)
     df.sort_values(by='DateTime')
     return df
 
@@ -25,8 +27,15 @@ unique_indicators = data.indicator.unique()
 print unique_indicators
 #create a data frame dictionary to store your data frames
 df_dict = {elem : pd.DataFrame for elem in unique_indicators}
+predict_dict = {elem : pd.DataFrame for elem in unique_indicators}
 
 for key in df_dict.keys():
     df_dict[key] = data[:][data.indicator == key]
-    
-print df_dict['GTG1'].head()
+    df_dict[key] = df_dict[key].drop(['DateTime','indicator'], axis=1)
+    clf = IsolationForest()
+    clf.fit(df_dict[key])
+    predict_dict[key] = clf.predict(df_dict[key])
+    df = df_dict[key]
+    df['outlier'] = predict_dict[key]
+    print df.head()
+    df.to_csv(key+'.csv', sep=',')
