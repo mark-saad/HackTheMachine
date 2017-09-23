018d8ff6a5c9c0cc91214a64918cd44efe93d0c9
Mark Saad <markadel@gmail.com>
2017-09-23 15:04:04 -0400

    improve sorting and plotting

--- run_me.py
+++ run_me.py
@@ -9,33 +9,32 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import IsolationForest
 
-"""
 def read_csv(filepath):
-    data = np.genfromtxt(filepath, delimiter=',')
-    return data
-"""
-def read_csv(filepath):
-    df = pd.read_csv(filepath)
+    df = pd.read_csv(filepath, parse_dates=True, index_col='DateTime')
     df.fillna(0, inplace=True)
-    df.sort_values(by='DateTime')
+    df.sort_index()
     return df
 
 
 data = read_csv("D:\HackTheMachine\hackthemachine-data\classA_ship1_allGTG.csv")
 #print data.head()
+
 unique_indicators = data.indicator.unique()
-print unique_indicators
+#print unique_indicators
 #create a data frame dictionary to store your data frames
 df_dict = {elem : pd.DataFrame for elem in unique_indicators}
 predict_dict = {elem : pd.DataFrame for elem in unique_indicators}
 
 for key in df_dict.keys():
     df_dict[key] = data[:][data.indicator == key]
-    df_dict[key] = df_dict[key].drop(['DateTime','indicator'], axis=1)
+    df_dict[key] = df_dict[key].drop(['indicator'], axis=1)
     clf = IsolationForest()
     clf.fit(df_dict[key])
     predict_dict[key] = clf.predict(df_dict[key])
     df = df_dict[key]
-    df['outlier'] = predict_dict[key]
-    print df.head()
-    df.to_csv(key+'.csv', sep=',')
+    df['normal'] = predict_dict[key]
+    #print df.head()
+    ax = df.plot(kind='line',logy=True).legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    fig = ax.get_figure()
+    fig.savefig(key+'.png')
+    #df.to_csv(key+'.csv', sep=',')
